48d7134c3d70bc3b7b08f1db94bf7ee1008903bd
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-08-20 17:25:54 +0200

    Added runtime config

---
+++ config.runtime.js
@@ -0,0 +1,52 @@
+module.exports = {git 
+    backends: {
+        'Bitcoin (BTC)': [{
+            name: 'TREZOR Wallet - Bitcoin',
+            urls: ['https://btc1.trezor.io', 'https://btc2.trezor.io', 'https://btc3.trezor.io', 'https://btc4.trezor.io', 'https://btc5.trezor.io', ],
+            explorer: '',
+        }, ],
+        'Bitcoin Cash (BCH)': [{
+            name: 'TREZOR Wallet - Bitcoin Cash',
+            urls: ['https://bch1.trezor.io', 'https://bch2.trezor.io', 'https://bch3.trezor.io', 'https://bch4.trezor.io', 'https://bch5.trezor.io', ],
+            explorer: '',
+        }, ],
+        'Bitcoin Gold (BTG)': [{
+            name: 'TREZOR Wallet - Bitcoin Gold',
+            urls: ['https://btg1.trezor.io', 'https://btg2.trezor.io', 'https://btg3.trezor.io', 'https://btg4.trezor.io', 'https://btg5.trezor.io', ],
+            explorer: '',
+        }, ],
+        'Dash (DASH)': [{
+            name: 'TREZOR Wallet - Dash',
+            urls: ['https://dash1.trezor.io', 'https://dash2.trezor.io', 'https://dash3.trezor.io', 'https://dash4.trezor.io', 'https://dash5.trezor.io', ],
+            explorer: '',
+        }, {
+            name: 'Dash.org',
+            urls: ['https://insight.dash.org', ],
+            explorer: 'https://insight.dash.org/insight/',
+        }, ],
+        'Dogecoin (DOGE)': [{
+            name: 'TREZOR Wallet - Dogecoin',
+            urls: ['https://doge1.trezor.io', 'https://doge2.trezor.io', 'https://doge3.trezor.io', 'https://doge4.trezor.io', 'https://doge5.trezor.io', ],
+            explorer: '',
+        }, ],
+        'Litecoin (LTC)': [{
+            name: 'TREZOR Wallet - Litecoin',
+            urls: ['https://ltc1.trezor.io', 'https://ltc2.trezor.io', 'https://ltc3.trezor.io', 'https://ltc4.trezor.io', 'https://ltc5.trezor.io', ],
+            explorer: '',
+        }, {
+            name: 'Litecore.io',
+            urls: ['https://insight.litecore.io/', ],
+            explorer: '',
+        }, ],
+        'Vertcoin (VTC)': [{
+            name: 'TREZOR Wallet - Vertcoin',
+            urls: ['https://vtc1.trezor.io', 'https://vtc2.trezor.io', 'https://vtc3.trezor.io', 'https://vtc4.trezor.io', 'https://vtc5.trezor.io', ],
+            explorer: '',
+        }, ],
+        'Zcash (ZEC)': [{
+            name: 'TREZOR Wallet - Zcash',
+            urls: ['https://zec1.trezor.io', 'https://zec2.trezor.io', 'https://zec3.trezor.io', 'https://zec4.trezor.io', 'https://zec5.trezor.io', ],
+            explorer: '',
+        }, ],
+    },
+};
