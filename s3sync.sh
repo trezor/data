0197dfe0e25906e17eae542296f87e506f34deaf
slush0 <slush@satoshilabs.com>
2018-10-19 15:07:12 +0200

    s3sync.sh

---
+++ s3sync.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Before first use:
+# Install awscli (pip install awscli)
+# Configure access credentials (aws configure), region is "eu-central-1"
+
+DIRS="bootloader bridge firmware udev"
+BUCKET=data.trezor.io
+DISTRIBUTION_ID="E1ERY5K2OTKKI1"
+
+set -e
+cd `dirname $0`
+
+read -r -p "Are you sure? [y/N] " response
+if [[ $response =~ ^(yes|y)$ ]]; then
+    echo "let's go!"
+else
+    exit 2
+fi
+
+for DIR in $DIRS; do
+    if [ "x$1" == "x-d" ]; then
+        aws s3 sync --delete --cache-control 'public, max-age=3600' $DIR s3://$BUCKET/$DIR
+    else
+        aws s3 sync --cache-control 'public, max-age=3600' $DIR s3://$BUCKET/$DIR
+    fi
+done
+
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths '/*'
+
+echo "DONE"
