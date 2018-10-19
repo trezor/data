961b823b2375e6dcb8d4af0e7c7203571928dbdd
slush0 <slush@satoshilabs.com>
2018-10-19 16:03:24 +0200

    s3sync: Run check_releases.py before sync.

--- s3sync.sh
+++ s3sync.sh
@@ -8,8 +8,15 @@ DIRS="bootloader bridge firmware udev"
 BUCKET=data.trezor.io
 DISTRIBUTION_ID="E1ERY5K2OTKKI1"
 
-set -e
 cd `dirname $0`
+
+./check_releases.py
+if [ "$?" != "0" ]; then
+    echo "check_releases.py failed."
+    exit
+fi
+
+set -e
 
 read -r -p "Are you sure? [y/N] " response
 if [[ $response =~ ^(yes|y)$ ]]; then
