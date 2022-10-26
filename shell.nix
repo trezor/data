31e32fab5065bc6d671bb058284284f2e95615db
Pavol Rusnak <pavol@rusnak.io>
2022-10-26 13:19:49 +0200

    chore: update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,13 +1,13 @@
-# the last successful build of nixos-21.05 (stable) as of 2021-08-02
+# the last successful build of nixos-22.05 (stable) as of 2022-10-26
 with import
   (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/16bf3980bfa0d8929639be93fa8491ebad9d61ec.tar.gz";
-    sha256 = "0azsnd2pjg53siv97n5l62j0c8b5whi5bd5a2wqz1sphkirf3cgq";
+    url = "https://github.com/NixOS/nixpkgs/archive/6107f97012a0c134c5848125b5aa1b149b76d2c9.tar.gz";
+    sha256 = "1n3r4s53q2clynvd6v2css054kf0icyfhxgs79brqvmrsxa7d0db";
   })
 { };
 
 stdenv.mkDerivation {
-  name = "webwallet-data";
+  name = "trezor-data";
   buildInputs = [
     bash
     awscli
