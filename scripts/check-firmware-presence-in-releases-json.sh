9950ae4dbe381a1d8163f01647444f6766baaf30
Peter Sanderson <dierk.deidel@protonmail.com>
2024-08-08 12:33:44 +0200

    fix: rework the algorithm so it takes the  from releases.json into an account, this allows a cross-directory links
    fix: binary missing in 2 directory + fix inconsistend url in releases.json
    
    fix: rework the algorithm so it takes the  from releases.json into an account, this allows a cross-directory links

--- scripts/check-firmware-presence-in-releases-json.sh
+++ scripts/check-firmware-presence-in-releases-json.sh
@@ -2,7 +2,6 @@
 
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
 
-GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
@@ -14,47 +13,52 @@ fi
 
 DEVICE=$1
 
-extract_filenames_from_json() {
+extract_file_paths_from_json() {
   local json_file="$1"
 
-  # Filter out 
-  #   a) 'null' from missing .url_bitcoinonly for older firmwares
-  #   b) super-old firmwares
+  # Filter out 'null' from missing .url_bitcoinonly for older firmwares
 
-  jq -r '.[] | select(.url) | .url, .url_bitcoinonly' "$json_file" | xargs -n 1 basename | sort | uniq \
+  jq -r '.[] | select(.url) | .url, .url_bitcoinonly' "$json_file" | xargs -n1 --no-run-if-empty | sort | uniq \
     | grep -vF "null"
 }
 
 list_files_in_directory() {
   local dir="$1"
   find "$dir" -type f -name "*.bin" -exec basename {} \; | sort \
-    | grep -v "trezor-inter-" | grep -v "trezor-t1tb-inter-" # filer out Intermediary firmwares
+    | grep -v "trezor-inter-" | grep -v "trezor-t1tb-inter-" # Filter out Intermediary firmwares
 }
 
 compare_files() {
   local json_file="$1"
   local directory="$2"
 
-  expected_files=$(extract_filenames_from_json "$json_file")
-  actual_files=$(list_files_in_directory "$directory")
+  # TEST 1: All files in releases.json exist
 
-  missing_files=$(comm -23 <(echo "$expected_files") <(echo "$actual_files"))
-  extra_files=$(comm -13 <(echo "$expected_files") <(echo "$actual_files"))
+  files_in_releases_json=$(extract_file_paths_from_json "$json_file")
 
-  if [[ -z "$missing_files" && -z "$extra_files" ]]; then
-    echo -e "${GREEN}All files are present and accounted for.${NC}"
-  else
-    if [[ -n "$missing_files" ]]; then
-      echo -e "${RED}Missing files:"
-      echo "$missing_files" | awk '{print "    " $0}'
-      echo -e "${NC}"
-    fi
-    if [[ -n "$extra_files" ]]; then
-      echo -e "${RED}Extra files in directory:"
-      echo "$extra_files" | awk '{print "    " $0}'
-      echo -e "${NC}"
-    fi
+  all_exist=true
+  for file in $files_in_releases_json; do
+      file_to_test="$directory/../../../$file"
+      if [ ! -e "$file_to_test" ]; then
+          echo -e "${RED}File does not exist: $file_to_test${NC}"
+          all_exist=false
+      fi
+  done
 
+  if ! $all_exist ; then
+      exit 1
+  fi
+
+  # TEST 2: All files in directory are in releases.json
+
+  actual_files=$(list_files_in_directory "$directory") # All files in the directory
+  full_path_actual_files=$(for i in $actual_files; do echo "data/firmware/${DEVICE}/${i}"; done) # Prefixed to match the expected format in releases.json
+  extra_files=$(comm -13 <(echo "$files_in_releases_json") <(echo "$full_path_actual_files"))
+
+  if [[ -n "$extra_files" ]]; then
+    echo -e "${RED}Extra files in directory:"
+    echo "$extra_files" | awk '{print "    " $0}'
+    echo -e "${NC}"
     exit 1
   fi
 }
