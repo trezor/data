e51806a6e1f23100b92f7f5d25323382d69a8e72
karliatto <yo@karliatto.com>
2025-07-25 13:53:00 +0200

    fix(scripts): update scripts to check also new releases JSON

--- scripts/check-all-firmware-revisions.sh
+++ scripts/check-all-firmware-revisions.sh
@@ -13,11 +13,18 @@ cd "$PARENT_PATH/../firmware" || exit
 
 EXCLUDED_DIR="translations"
 
+IS_LEGACY="false"
+
 for dir in */ ; do
     DEVICE=${dir%/}
-    if [[ $EXCLUDED_DIR == "$DEVICE" ]]; then continue; fi
+    # TODO: we excude t3w1 until we have releases on it.
+    if [[ $EXCLUDED_DIR == "$DEVICE" || $DEVICE == "t3w1" ]]; then continue; fi
+        is_legacy_for_device="$IS_LEGACY"
+    if [[ "$DEVICE" == "1" || "$DEVICE" == "2" ]]; then
+        is_legacy_for_device="true"
+    fi
     if [ -d "$dir" ]; then
-        if ! "$PARENT_PATH/check-firmware-revisions.sh" "$DEVICE" ; then
+        if ! "$PARENT_PATH/check-firmware-revisions.sh" "$DEVICE" "$is_legacy_for_device" ; then
             exit 1
         fi;
     fi
